3cd1c2b270f57e9c4f8392c8e3fcb0bb76ddb213
韩承海 <374802597@qq.com>
2018-01-29 19:18:11 +0800

    no message

--- ReactSwift/ReactSwift/DemoVC/ReactorKitViewController.swift
+++ ReactSwift/ReactSwift/DemoVC/ReactorKitViewController.swift
@@ -10,32 +10,42 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-//行为
-enum Action {
-    case query(String?)
-    case loadNextPage
-}
-
-//状态变更 --> 驱动状态变化
-enum Mutation {
-    case setQuery(String?)
-    case setRepos([String], nextPage: Int?)
-    case setLoadingNextPage(Bool)
-}
-
-//状态 --> 更新UI
-struct State {
-    var query: String?
-    var repos: [String] = []
-    var nextPage: Int?
-    var isLoadingNextPage: Bool = false
-}
 
 
 
 
-class ReactorKitViewController: UIViewController {
+final class ReactorKitViewController: UIViewController {
+    
+    
+    //用户行为
+    enum Action {
+        case updateQuery(String?)
+        case loadNextPage
+    }
+    
+    //状态变更 --> 驱动状态变化
+    enum Mutation {
+        case setQuery(String?)
+        case setRepos([String], nextPage: Int?)
+        case addRepos([String], nextPage: Int?)
+        case setLoadingNextPage(Bool)
+    }
+    
+    //状态 --> 更新UI
+    struct State {
+        var query: String?
+        var repos: [String] = []
+        var nextPage: Int?
+        var isLoadingNextPage: Bool = false
+    }
 
+
+    let initialState = State()
+    
+    
+
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
 
